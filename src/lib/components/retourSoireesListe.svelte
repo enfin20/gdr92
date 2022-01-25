b3d0698b039ebbc915cc8889fa82250e8b047d3f
enfin20 <96887337+enfin20@users.noreply.github.com>
2022-01-25 15:48:03 +0100

    present/absent

--- src/lib/components/retourSoireesListe.svelte
+++ src/lib/components/retourSoireesListe.svelte
@@ -4,62 +4,173 @@
 	import { date_dd_MM_YYYY } from '$lib/date_functions';
 </script>
 
-<div class="table text-center text-sm ">
-	<div class="table-header-group font-bold">
-		<div class=" table-cell text-left w-2/12 align-middle py-1 px-1">Soirée</div>
-		<div class=" table-cell text-left w-1/12 align-middle py-1 px-1">Responsable</div>
-		<div class=" table-cell w-1/12 align-middle py-1 px-1">
-			<img
-				src="https://www.orientsport.fr/oflash/img/gare.png"
-				class=""
-				alt="gare"
-				style="display: inline"
-				width="36px"
-				height="36px"
-			/>
-		</div>
-		<div class=" table-cell w-1/12 align-middle py-1 px-1">
-			<img
-				src="https://www.orientsport.fr/oflash/img/gp.png"
-				class=""
-				alt="Gabriel Péri"
-				style="display: inline"
-				width="36px"
-				height="36px"
-			/>
-		</div>
-		<div class=" table-cell w-1/12 align-middle py-1 px-1">
-			<img
-				src="https://www.orientsport.fr/oflash/img/entrepot.png"
-				class=""
-				alt="Total"
-				style="display: inline"
-				width="36px"
-				height="36px"
-			/>
-		</div>
-		<div class=" table-cell w-6/12  text-left align-middle py-1 px-1">Commentaires</div>
-	</div>
-	{#each retourSoirees as cell}
-		<div class="table-row-group align-middle">
-			<div class=" table-cell text-left w-2/12 align-middle py-1 px-1">
-				{date_dd_MM_YYYY(cell.soiree).date}
-			</div>
-			<div class=" table-cell text-left w-1/12 align-middle py-1 px-1">
-				{cell.rs.substring(0, cell.rs.indexOf(' ') + 2) + '.'}
-			</div>
-			<div class=" table-cell w-1/12 align-middle py-1 px-1">
-				{cell.nbGare}
-			</div>
-			<div class=" table-cell w-1/12 align-middle py-1 px-1">
-				{cell.nbPeri}
-			</div>
-			<div class=" table-cell w-1/12 align-middle py-1 px-1 font-bold">
-				{Number(cell.nbGare) + Number(cell.nbPeri)}
-			</div>
-			<div class=" table-cell w-6/12  text-left align-middle py-1 px-1">
-				{cell.commentaires}
-			</div>
-		</div>
-	{/each}
-</div>
+<table id="RetourSoirees" class="text-sm text-gray-500">
+	<thead>
+		<tr>
+			<th class="w-[15%]" />
+			<th class="w-[10%]" />
+			<th colspan="3" class="w-[10%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/entrepot.png"
+						class=""
+						alt="repas"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th colspan="2" class="w-[10%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/present.png"
+						class=""
+						alt="present"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th colspan="2" class="w-[10%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/absent.png"
+						class=""
+						alt="gare"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-1/2" />
+		</tr>
+		<tr>
+			<th class="text-left font-bold w-[15%]">Soirée</th>
+			<th class="text-left font-bold w-[10%]">Responsable</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gare.png"
+						class=""
+						alt="gare"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gp.png"
+						class=""
+						alt="gp"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/repas.png"
+						class=""
+						alt="total"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gare.png"
+						class=""
+						alt="gare"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gp.png"
+						class=""
+						alt="gp"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gare.png"
+						class=""
+						alt="gare"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="w-[5%]">
+				<div class="flex items-center justify-center">
+					<img
+						src="https://www.orientsport.fr/oflash/img/gp.png"
+						class=""
+						alt="gp"
+						style="display: inline"
+						width="36px"
+						height="36px"
+					/>
+				</div>
+			</th>
+			<th class="text-left font-bold">Commentaires</th>
+		</tr>
+	</thead>
+	<tbody class="divide-y divide-gray-100">
+		{#each retourSoirees as cell}
+			<tr class="align-middle divide-y divide-gray-100 hover:bg-slate-100 text-center">
+				<td class="text-left align-middle py-1 px-1">
+					{date_dd_MM_YYYY(cell.soiree).date}
+				</td>
+				<td class="text-left align-middle py-1 px-1">
+					{cell.rs.substring(0, cell.rs.indexOf(' ') + 2) + '.'}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.nbGare}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.nbPeri}
+				</td>
+				<td class=" align-middle py-1 px-1 font-bold w-[5%]">
+					{Number(cell.nbGare) + Number(cell.nbPeri)}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.gare_present}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.gp_present}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.gare_absent}
+				</td>
+				<td class="align-middle py-1 px-1 w-[5%]">
+					{cell.gp_absent}
+				</td>
+				<td class=" text-left align-middle py-1 px-1">
+					{cell.commentaires}
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
--- src/routes/retourSoirees/index.js
+++ src/routes/retourSoirees/index.js
@@ -3,16 +3,111 @@ import { ObjectId } from 'mongodb';
 import { YYYYMMDD } from '$lib/date_functions';
 
 export async function get(request) {
-	// récupération des données d'une soirée par date (YYYYMMDD)
+	// récupération des données des anciennes soirees
 	try {
+		// pour compter le nombre de présents par lieu
+		const pipeline_presents = [
+			{
+				$match: {
+					statut: {
+						$in: ['Oui', 'RS', 'Vaisselle']
+					},
+					lieu: {
+						$in: ['gp', 'gare']
+					},
+					soiree: { $lte: YYYYMMDD(0).date }
+				}
+			},
+			{
+				$group: {
+					_id: {
+						soiree: '$soiree',
+						lieu: '$lieu'
+					},
+					present: {
+						$sum: 1
+					}
+				}
+			},
+			{
+				$sort: {
+					_id: 1
+				}
+			}
+		];
+
+		// pour compter le nombre d'absents par lieu
+		const pipeline_absents = [
+			{
+				$match: {
+					statut: 'Absent',
+					lieu: {
+						$in: ['gp', 'gare']
+					},
+					soiree: { $lte: YYYYMMDD(0).date }
+				}
+			},
+			{
+				$group: {
+					_id: {
+						soiree: '$soiree',
+						lieu: '$lieu'
+					},
+					absent: {
+						$sum: 1
+					}
+				}
+			},
+			{
+				$sort: {
+					_id: 1
+				}
+			}
+		];
+
 		const dbConnection = await connectToDatabase();
 		const db = dbConnection.db;
 		const collection = db.collection('RetourSoirees');
+		const coll2 = db.collection('CalendrierBenevoles');
 		const retourSoirees = await collection
 			.find({ soiree: { $lte: YYYYMMDD(0).date } })
 			.sort({ soiree: -1 })
 			.toArray();
 
+		const presents = await coll2.aggregate(pipeline_presents).toArray();
+
+		console.log('presents : ' + presents[0]._id.soiree);
+		console.log('retourSoiree : ' + retourSoirees[0].soiree);
+
+		const absents = await coll2.aggregate(pipeline_absents).toArray();
+
+		// rajput des presents & absents à retour soirée
+		for (var i = 0; i < retourSoirees.length; i++) {
+			for (var j = 0; j < presents.length; j++) {
+				if (retourSoirees[i].soiree === presents[j]._id.soiree) {
+					if (presents[j]._id.lieu === 'gp') {
+						retourSoirees[i].gp_present = presents[j].present;
+					}
+					if (presents[j]._id.lieu === 'gare') {
+						retourSoirees[i].gare_present = presents[j].present;
+					}
+				}
+			}
+			retourSoirees[i].gp_absent = 0;
+			retourSoirees[i].gare_absent = 0;
+			for (var k = 0; k < absents.length; k++) {
+				if (retourSoirees[i].soiree === absents[k]._id.soiree) {
+					if (absents[k]._id.lieu === 'gp') {
+						retourSoirees[i].gp_absent = absents[k].absent;
+					}
+					if (absents[k]._id.lieu === 'gare') {
+						retourSoirees[i].gare_absent = absents[k].absent;
+					}
+				}
+			}
+		}
+
+		console.log('soiree present : ' + retourSoirees[0].gp_present);
 		return {
 			status: 200,
 			body: {
