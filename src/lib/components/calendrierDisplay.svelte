0c684c8a3182fa3fd9519b435f26957cd940a00d
enfin20 <96887337+enfin20@users.noreply.github.com>
2022-02-01 07:41:08 +0100

    Update calendrierDisplay.svelte

--- src/lib/components/calendrierDisplay.svelte
+++ src/lib/components/calendrierDisplay.svelte
@@ -5,48 +5,52 @@
 </script>
 
 <!-- table pour les impressions -->
-<table id="tableCalendrier" class="text-sm text-gray-500 w-full">
-	<thead>
-		<tr>
-			{#each dates as cell}
-				<th>{cell}</th>
-			{/each}
-		</tr>
-		<tr class="">
-			{#each lieux as cell}
-				<th> <div class="flex items-center justify-center">{@html cell}</div></th>
-			{/each}
-		</tr>
-	</thead>
-	<tbody class="divide-y divide-gray-100">
-		{#each calendriers as row}
-			<tr class="hover:bg-slate-100 text-center">
-				{#each row as cell}
-					{#if typeof cell._id != 'undefined'}
-						<td
-							class={cell.presence === 'Oui'
-								? 'bg-green-400  text-gray-500 py-1 px-1 '
-								: cell.presence === 'RS'
-								? 'bg-pink-300  text-gray-500 py-1 px-1 '
-								: cell.presence === 'Dispo'
-								? 'bg-gray-200  text-gray-600 py-1 px-1 '
-								: cell.presence === 'Maraude'
-								? 'bg-yellow-400  text-gray-500 py-1 px-1 '
-								: cell.presence === 'Vaisselle'
-								? 'bg-blue-300  text-gray-500 py-1 px-1 '
-								: cell.presence === 'Absent'
-								? 'bg-red-500  text-gray-200 py-1 px-1 '
-								: 'text-gray-500 py-1 px-1 '}
-						>
-							{cell.presence.substring(0, 3)}
-						</td>
-					{:else}
-						<td class="text-gray-500 py-1 px-1 text-left">
-							{cell.benevole}
-						</td>
-					{/if}
+<div class="flex flex-col h-screen">
+	<div class="flex-grow overflow-auto">
+		<table id="tableCalendrier" class="text-sm text-gray-500 w-full relative">
+			<thead>
+				<tr>
+					{#each dates as cell}
+						<th class="sticky top-0 bg-white">{cell}</th>
+					{/each}
+				</tr>
+				<tr class="">
+					{#each lieux as cell}
+						<th> <div class="flex items-center justify-center">{@html cell}</div></th>
+					{/each}
+				</tr>
+			</thead>
+			<tbody class="divide-y divide-gray-100">
+				{#each calendriers as row}
+					<tr class="hover:bg-slate-100 text-center">
+						{#each row as cell}
+							{#if typeof cell._id != 'undefined'}
+								<td
+									class={cell.presence === 'Oui'
+										? 'bg-green-400  text-gray-500 py-1 px-1 '
+										: cell.presence === 'RS'
+										? 'bg-pink-300  text-gray-500 py-1 px-1 '
+										: cell.presence === 'Dispo'
+										? 'bg-gray-200  text-gray-600 py-1 px-1 '
+										: cell.presence === 'Maraude'
+										? 'bg-yellow-400  text-gray-500 py-1 px-1 '
+										: cell.presence === 'Vaisselle'
+										? 'bg-blue-300  text-gray-500 py-1 px-1 '
+										: cell.presence === 'Absent'
+										? 'bg-red-500  text-gray-200 py-1 px-1 '
+										: 'text-gray-500 py-1 px-1 '}
+								>
+									{cell.presence.substring(0, 3)}
+								</td>
+							{:else}
+								<td class="text-gray-500 py-1 px-1 text-left">
+									{cell.benevole}
+								</td>
+							{/if}
+						{/each}
+					</tr>
 				{/each}
-			</tr>
-		{/each}
-	</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+</div>
