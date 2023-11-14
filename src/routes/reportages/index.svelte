c3a5919894aeac84634b506d621560c0289960f3
enfin20 <96887337+enfin20@users.noreply.github.com>
2023-11-14 20:02:54 +0100

    reportages

---
+++ src/routes/reportages/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+</script>
+
+<script>
+	const source = '/videos/BFM-Paris-le-mardi-14-novembre-2023-07-01 1.mp4';
+</script>
+
+<svelte:head>
+	<title>Reportages Colombes</title>
+</svelte:head>
+<h1>Reportage BFM 13/11/2023</h1>
+<video src={source} height="400" width="600" controls autoplay>
+	<track default kind="captions" srclang="fr" src="" />
+</video>
